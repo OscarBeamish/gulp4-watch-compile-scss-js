a884500cb6aaa5aa9c5dab3407771470cf16e9e2
OscarTBeamish <oscartbeamish@gmail.com>
2023-02-14 16:52:05 +0000

    Adding gulp-clean to delete files before being compiled

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 // Gulp Modules
-const { src, dest, watch, series, parallel } = require('gulp');
-const minify = require('gulp-minify')
-const sass = require('gulp-sass')(require('sass'));
+const { src, dest, watch, series, parallel } = require('gulp'),
+  minify = require('gulp-minify'),
+  sass = require('gulp-sass')(require('sass')),
+  clean = require('gulp-clean');
+
 
 
 // File paths
@@ -15,8 +17,21 @@ const files = {
 	jsDest: 'public/build/js/scripts.min.js',
 }
 
+// Delete minified CSS
+function scssClean() {
+  return src([files.scssDest])
+    .pipe(clean())
+}
+
+// Delete minified JavaScript
+function jsClean() {
+  return src([files.jsDest])
+    .pipe(clean())
+}
+
 // Package, compile and minify Sass
 function scssTask() {
+
   return src([files.scssSrc])
       .pipe(sass({outputStyle: 'compressed'}))
       .pipe(dest(files.scssDest))
@@ -24,6 +39,7 @@ function scssTask() {
 
 // Package and minify JavaScript
 function jsTask() {
+
   return src([files.jsSrc])
     .pipe(minify({
         ext:{
@@ -39,9 +55,9 @@ function jsTask() {
 function watchTask() {
 	watch(
 		[files.scssWatch, files.jsWatch],
-		series(parallel(scssTask, jsTask))
+		series(scssClean, jsClean, parallel(scssTask, jsTask))
 	)
 }
 
 
-exports.default = series(parallel(scssTask, jsTask), watchTask);
+exports.default = series(scssClean, jsClean, parallel(scssTask, jsTask), watchTask);
